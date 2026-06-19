840d3a964c8708678cba95863d78cd2a7e63dd34
vibhag2005 <vibhashree2005@gmail.com>
2026-06-19 11:11:49 +0530

    Remove headless options from ChromeOptions
    Removed headless options from ChromeOptions instantiation.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,15 +3,9 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
 
         WebDriver driver = new ChromeDriver(options);
 
